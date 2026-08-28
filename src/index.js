615106ae2c738b9098171d970e5d3526466c4e88
ROYAL <reedemroyal@gmail.com>
2026-08-28 13:58:54 +0530

    Fix guild command registration

--- src/index.js
+++ src/index.js
@@ -228,8 +228,9 @@ client.once(
         );
 
       await rest.put(
-        Routes.applicationCommands(
-          process.env.CLIENT_ID
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID,
+          process.env.GUILD_ID
         ),
         {
           body: commands
